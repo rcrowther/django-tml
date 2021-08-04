8135501b038c78f6583f5f3cf1a5ff5a201b7b75
rcrowther <rw.crowther@gmail.com>
2021-08-04 08:47:03 +0100

    Add new img tag Update README

--- tml/__init__.py
+++ tml/__init__.py
@@ -1,5 +1,5 @@
 """A reliable markup language with the essentials for HTML typography. Fast to type and memorable. Includes optional UML, for unicode shortcuts."""
-__version__ = '0.6.3'
+__version__ = '0.6.4'
 
 from .io import html
 
