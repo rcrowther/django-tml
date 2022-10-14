8a0e5b32e59794e9c55f1accd3e13ba720026897
rcrowther <rw.crowther@gmail.com>
2022-10-14 20:19:16 +0100

    Bump major version

--- tml/__init__.py
+++ tml/__init__.py
@@ -1,5 +1,5 @@
 """A reliable markup language with the essentials for HTML typography. Fast to type and memorable. Includes optional UML, for unicode shortcuts."""
-__version__ = '0.6.4'
+__version__ = '1.0.0'
 
 from .io import html
 
